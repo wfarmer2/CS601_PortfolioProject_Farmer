69593ff8f2891efba6633b00f2902a60560c840f
Will Farmer <wfarmer2@bu.edu>
2022-12-09 00:37:23 -0700

    Update styling, pages, javascript functions, and db file

---
+++ javascript/fetchData.js
@@ -0,0 +1,56 @@
+/**
+ * function fetches web server to pull requested information that is being hosted.
+ * @returns promise data from fetch request
+ */
+async function getURLInfo(url) {
+
+    // url variable of where the JSON file is being hosted on via Glitch
+    let requestedURL = (url);
+
+    try {
+        // call fetch on the url and await the return of the promise and store it in a 
+        // response variable
+        let response = await fetch(requestedURL);
+
+        // log the status code of the response
+        console.log('status code: ', response.status);
+
+        // check to see if the status code is valid - should be 200-299
+        // if repsonse is not valid then log to console and throw an error
+        if (!response.ok) {
+            console.log(response);
+            // response has an invalid status to fetch data from
+            throw new Error(`Error! status: ${response.status}`);
+          }
+        else {
+            // else if the status is ok then return the promise using 
+            // response.json()
+            return await response.json();
+        }
+    }
+    // catch any other errors that might occur and log to console
+    catch (error) {
+        console.log(error);
+    }
+}
+
+/**
+ * loadDegreeInfo function calls getDegreeInfo to grab JSON data from web server and 
+ * manipulates the DOM to insert into the table on the page.
+ */
+async function loadAPOD() {
+
+    // create degrees variable and call getDegreeInfo() function
+    // to fetch JSON data from server
+    let apodData = await getURLInfo("https://go-apod.herokuapp.com/apod");
+    // html code to be inserted into page
+    const html = `<h3>${apodData.title}</h3>
+                  <img src=${apodData.hdurl} height=250 width=350>
+                  <p>${apodData.date} - ${apodData.copyright}<br></p>
+                  <p>${apodData.explanation}</p>`
+                  
+    
+    // grab the current table and add each degree data to it
+    let div = document.getElementById("apod");
+    div.innerHTML = html;
+}
---
+++ javascript/validateContact.js
@@ -0,0 +1,86 @@
+// constant varibale for A-z characters to use for regex checks on form
+const letterPattern = /^[A-Za-z]+$/;
+
+
+/**
+ * validateForm function is a wrapper function to call each validation function for the program
+ * @param event is the action being done on the form
+ * @returns an alert if there is a failure in any of the validation checks
+ */
+function validateForm(event) {
+    // prevent any default action of the form to control how errors and flow is being handled
+    event.preventDefault();
+    
+    // assign value of each function to a variable for conditional check
+    let validFirstName = validateFirstName();
+    let validLastName = validateLastName();
+
+    // if any of the values are false then throw an alert to user so they can fix the errors before submission
+    if (!validFirstName || !validLastName) {
+        return alert("Please correct form errors before submission.");
+    }
+
+    // validated form can be submitted to destination 
+    event.target.submit();
+
+}
+
+/**
+ * validateFirstName checks First Name input into form to see if it is valid or not 
+ * @returns true if there are no errors, or false if there are errors based on the validation checks
+ */
+function validateFirstName() {
+    // grab input value from DOM
+    let firstName = document.getElementById("firstName");
+
+    // check to see if the input is more than or equal to 2 characters, not null, empty string or not
+    if (firstName.value.length <= 2 || firstName.value == null || firstName.value == "") {
+        // change message using DOM manipulation to show error and change text color to user
+        document.getElementById("firstNamePrompt").innerHTML += ("<span style='color:red'> Invalid name! First name must contain two (2) or more characters.</span>");
+        // focus on text input and return false to not submit form
+        firstName.focus();
+        return false;
+    }
+    // check to see if the input matches regex pattern defined at top of the file )only A-z characters)
+    if (!(firstName.value.match(letterPattern))) {
+        // change message using DOM manipulation to show error and change text color to user
+        document.getElementById("firstNamePrompt").innerHTML += ("<span style='color:red'> Invalid name! First name must contain only alphabetic characters.</span>");
+        // focus on text input and return false to not submit form
+        firstName.focus();
+        return false;
+    }
+    
+    // return true if input is valid
+    return true;   
+
+}
+
+/**
+ * validateLastName checks Last Name input into form to see if it is valid or not 
+ * @returns true if there are no errors, or false if there are errors based on the validation checks
+ */
+function validateLastName() {
+    // grab input value from DOM
+    let lastName = document.getElementById("lastName");
+
+    // check to see if the input is more than or equal to 2 characters, not null, empty string or not
+    if (lastName.value.length <= 2 || lastName.value == null || lastName.value == "") {
+        // change message using DOM manipulation to show error and change text color to user
+        document.getElementById("lastNamePrompt").innerHTML += ("<span style='color:red'> Invalid name! Last name must contain two (2) or more characters.</span>");
+        // focus on text input and return false to not submit form
+        lastName.focus();
+        return false;
+    }
+    // check to see if the input matches regex pattern defined at top of the file )only A-z characters)
+    if (!(lastName.value.match(letterPattern))) {
+        // change message using DOM manipulation to show error and change text color to user
+        document.getElementById("lastNamePrompt").innerHTML += ("<span style='color:red'> Invalid name! Last name must contain only alphabetic characters.</span>");
+        // focus on text input and return false to not submit form
+        lastName.focus();
+        return false;
+    }
+
+    // return true if input is valid
+    return true;
+}
+
